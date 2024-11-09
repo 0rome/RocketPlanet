3f3e5f9d02e7395f1a1949f8df89562ecd32b2ff
Nagward <0rome@mail.ru>
2024-11-09 16:11:08 +0300

    Добавленно сохранение кнопок покупки ракет

--- Assets/Scripts/BuyRockets.cs
+++ Assets/Scripts/BuyRockets.cs
@@ -6,12 +6,30 @@ using UnityEngine.UI;
 public class BuyRockets : MonoBehaviour
 {
     public GameObject RocketButton;
+    public GameObject PriceButton;
+
+    public string KEY;
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey(KEY)) { PlayerPrefs.SetInt(KEY,0); }
+        if (PlayerPrefs.GetInt(KEY) == 1 && PriceButton != null)
+        {
+            PriceButton.SetActive(false);
+            RocketButton.GetComponent<Button>().interactable = true;
+        }
+    }
     public void BuyRocket(int Price)
     {
         if (PlayerPrefs.GetInt("Coins") > Price)
         {
             RocketButton.GetComponent<Button>().interactable = true;
             PlayerPrefs.SetInt("Coins", PlayerPrefs.GetInt("Coins") - Price);
+
+            if (PriceButton != null)
+            {
+                PriceButton.SetActive(false);
+            }
         }
         else
         {
@@ -21,5 +39,8 @@ public class BuyRockets : MonoBehaviour
     public void PickRocket(int RocketNumber)
     {
         PlayerPrefs.SetInt("Player",RocketNumber);
+        
+        
+        PlayerPrefs.SetInt(KEY,1);
     }
 }
